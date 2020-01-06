5c91345cb4a0b4dc6fbe166a6b00b92ef7ba4d65
TrevorMcdougald <trevor.mcdougald@gmail.com>
2020-01-06 17:10:53 -0600

    I've decided that I need to decouple the client and server in the project structure completely. Things were getting too crossed with two node_modules folders and the IDE was getting pissed. I'm going to go back to the basics now...
    I started out grinding HTML, CSS, and JS/jQuery. Now I'm going to go back and do as much of the front end as I can before I need to implement the backend (which is already done and moved to a separate file location). I'll be focusing on React completely for the next couple weeks or so.

--- server/config/mongoose-promise.js
+++
@@ -1,6 +0,0 @@
-const Promise = require( 'bluebird' );
-const mongoose = require( 'mongoose' );
-
-mongoose.Promise = Promise;
-
-module.exports = mongoose;
--- server/tests/users.test.js
+++
@@ -1,4 +0,0 @@
-const request = require( 'supertest' );
-const { expect } = require( 'chai' );
-const app = require( '../app' );
-const { userInformation, userCredentials } = require( '../models' );
