1e89a8e632ae7e2d735556fa660621c566807638
TrevorMcdougald <trevor.mcdougald@gmail.com>
2020-02-28 09:20:07 -0600

    feat(README): Changing project demo files
    BREAKING CHANGE: None
    
    Closes None

--- src/state/ducks/user/actions.js
+++ src/state/ducks/user/actions.js
@@ -13,24 +13,26 @@ export const userLoggedOut = () => ({
   type: types.LOG_OUT,
 });
 
-export const login = (user, data) => dispatch =>
-  fetch(POST, `/${user}/auth/login`, data)
-    .then(({ data: { access_token } }) => {
-      localStorage.jwt_token = access_token;
-      dispatch(userLoggedIn(access_token));
-      dispatch(getUserData());
-      localStorage.removeItem("user_email");
-    })
-    .catch(error => {
-      dispatch(userLogInFail());
-
-      console.error(error);
-    });
+export const login = (user, data) => dispatch => {
+  dispatch
+};
+  // fetch(POST, `/${user}/auth/login`, data)
+  //   .then(({ data: { access_token } }) => {
+  //     localStorage.jwt_token = access_token;
+  //     dispatch(userLoggedIn(access_token));
+  //     dispatch(getUserData());
+  //     localStorage.removeItem("user_email");
+  //   })
+  //   .catch(error => {
+  //     dispatch(userLogInFail());
+  //
+  //     console.error(error);
+  //   });
 
 export const logout = () => dispatch => {
   localStorage.removeItem("jwt_token");
   localStorage.removeItem("lastProfileStep");
   localStorage.removeItem("username");
   dispatch(userLoggedOut());
-  history.push("/sign_in");
+  // history.push("/sign_in");
 };
