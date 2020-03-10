b99ce0881a02cdffea7a495d8a6b7199668c0308
TrevorMcdougald <trevor.mcdougald@gmail.com>
2020-03-10 11:19:19 -0500

    feat(Frontend): Redux and React-router sync
    Lots of stuff to say here:
    
    I figured out how to move the page to sync
    'react-router-dom' with 'react-redux'. However, I might want to look
    back into 'persist-react' in the future. I'm at the point where I really
    need to have a working backend so I can add async code to my actions and
    see how the application actually works.
    
    So now I want to launch my
    express backend with my application and test how it works. To do that I
    need to learn how to launch each separately on docker and how to host
    with AWS. That's what I'm going to spend the next week - two weeks
    focusing on: Purely deploying/hosting.
    
    BREAKING CHANGE: None
    
    Closes None

---
+++ src/state/ducks/orders/types.js
@@ -0,0 +1,6 @@
+export const CONFIRM_ORDER = 'CONFIRM_ORDER';
+export const CLEAR_ORDER = 'CLEAR_ORDER';
+export const CREATE_ORDER = 'CREATE_ORDER';
+export const ORDER_RECEIVED = 'ORDER_RECEIVED';
+export const ORDER_COMPLETE = 'ORDER_COMPLETE';
+export const FETCH_ORDERS = 'FETCH_ORDERS';
